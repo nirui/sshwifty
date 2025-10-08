4e13029f9279b384ff178555d43fbae4e3e73af2
Ni Rui <ranqus@gmail.com>
2025-10-08 11:23:58 +0800

    Bump copyright date

--- application/rw/rw.go
+++ application/rw/rw.go
@@ -1,6 +1,6 @@
 // Sshwifty - A Web SSH client
 //
-// Copyright (C) 2019-2023 Ni Rui <ranqus@gmail.com>
+// Copyright (C) 2019-2025 Ni Rui <ranqus@gmail.com>
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
